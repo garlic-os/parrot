fd5c094103bd272b4e7e560f9a019c73b2b2eccb
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2019-12-28 22:16:55 -0600

    I forgot to update this file
    embed.js no longer takes in any arguments when you require it. It figures out embedColors on its own.

--- embeds.js
+++ embeds.js
@@ -1,44 +1,46 @@
 "use strict"
 
 const RichEmbed = require("discord.js").RichEmbed
-
-module.exports = embedColors => {
-	return {
-		/**
-		 * Generate a Discord Rich Embed object out of the supplied information
-		 * 
-		 * @param {User} str - messages to print
-		 * @return {RichEmbed} Discord Rich Embed object
-		 */
-		standard: str => {
-			return new RichEmbed()
-				.setColor(embedColors.normal)
-				.addField("Schism", str)
-		},
-		
-
-		/**
-		 * Generate a Discord Rich Embed object out of the supplied information
-		 * 
-		 * @param {string} err - Error to print
-		 * @return {RichEmbed} Discord Rich Embed object
-		 */
-		error: err => {
-			return new RichEmbed()
-				.setColor(embedColors.error)
-				.addField("Error", err)
-		},
+const embedColors = (process.env.EMBED_COLORS)
+						? JSON.parse(process.env.EMBED_COLORS)
+						: require("./defaults").EMBED_COLORS
 
 
-		/**
-		 * A joke embed with xok
-		 * 
-		 * Only made once at startup because it's always the same
-		 */
-		xok: new RichEmbed()
+module.exports = {
+	/**
+	 * Generate a Discord Rich Embed object out of the supplied information
+	 * 
+	 * @param {User} str - messages to print
+	 * @return {RichEmbed} Discord Rich Embed object
+	 */
+	standard: str => {
+		return new RichEmbed()
+			.setColor(embedColors.normal)
+			.addField("Schism", str)
+	},
+
+
+	/**
+	 * Generate a Discord Rich Embed object out of the supplied information
+	 * 
+	 * @param {string} err - Error to print
+	 * @return {RichEmbed} Discord Rich Embed object
+	 */
+	error: err => {
+		return new RichEmbed()
+			.setColor(embedColors.error)
+			.addField("Error", err)
+	},
+
+
+	/**
+	 * A joke embed with xok
+	 * 
+	 * Only made once because it's always the same
+	 */
+	xok: new RichEmbed()
 			.attachFiles(["./img/xok.png"])
 			.setColor(embedColors.error)
 			.setTitle("Error")
 			.setImage("attachment://xok.png")
-	}
 }
