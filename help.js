f55c12b919901b0ebc2d500b2448af438fad5314
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-02-19 11:10:37 -0600

    Add descriptions for the new commands
    |servers, |speaking, and |learning have command entries now!

--- help.js
+++ help.js
@@ -1,41 +1,65 @@
 "use strict"
 
-const prefix = process.env.PREFIX || require("./defaults").PREFIX
+const prefix = process.env.prefix || require("./defaults").prefix
 
 module.exports = {
 	help: {
 		admin: false,
 		desc: `Help.`,
 		syntax: `${prefix}help`
-	},
-	imitate: {
+	}
+
+	, imitate: {
 		admin: false,
 		desc: `Imitate a user.`,
 		syntax: `${prefix}imitate <ping a user>`
-	},
-	save: {
+	}
+
+	, save: {
 		admin: true,
 		desc: `Upload all unsaved cache to S3.`,
 		syntax: `${prefix}save`
-	},
-	scrape: {
+	}
+
+	, scrape: {
 		admin: true,
 		desc: `Save [howManyMessages] messages in [channel].`,
 		syntax: `${prefix}scrape <channelID> <howManyMessages>`
-	},
-	embed: {
+	}
+
+	, embed: {
 		admin: true,
 		desc: `Generate an embed.`,
 		syntax: `${prefix}embed <message>`
-	},
-	error: {
+	}
+
+	, error: {
 		admin: true,
 		desc: `Generate an error embed.`,
 		syntax: `${prefix}error <message>`
-	},
-	xok: {
+	}
+
+	, xok: {
 		admin: true,
 		desc: `Xok`,
 		syntax: `${prefix}xok`
+	}
+
+	, servers: {
+		admin: false,
+		desc: `List all the servers Schism is a member of.`,
+		syntax: `${prefix}servers`
+	}
+
+	, speaking: {
+		admin: false,
+		desc: `List all the channels of a server that Schism can speak in.`,
+		syntax: `${prefix}speaking <serverID>`
+	}
+
+	, learning: {
+		admin: false,
+		desc: `List all the channels of a server that Schism is learning from.`,
+		syntax: `${prefix}learning <serverID>`
 	}
 }
