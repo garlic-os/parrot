8b61c701ecc55ba91a163bf5e8e6ec3f3f76056e
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-03-05 19:53:11 -0600

    Add regex pattern for emojis

--- schism/regex.js
+++ schism/regex.js
@@ -4,11 +4,22 @@ module.exports = {
 	 * An ! exclamation point is present when the user has a nickname
 	 *   on the server the message was sent from.
 	 * An & ampersand is present when it is a role mention, like @moderators.
+	 * 
+	 * @type {RegExp}
 	 */
 	mention: /<@([!&]*)[0-9]+>/g
 
 	/**
 	 * The string of numbers inside an instance of <@[userID]>.
+	 * 
+	 * @type {RegExp}
 	 */
 	, id: /[0-9]+/
+
+	/**
+	 * Standard and custom emojis.
+	 * 
+	 * @type {RegExp}
+	 */
+	, emoji: /:\w+[:>]/g
 }
