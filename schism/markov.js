c3db7bb69bed1230e7eeeba1bb26ff102534981f
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-04-09 17:07:49 -0500

    Revert to old Markov algorithm
    Aaaah! It was so slow! This algorithm takes much longer to build than the old one, so I'm changing it back. This will probably not be a problem when markov-strings updates to v3, but until then, I can't use it.

--- schism/markov.js
+++ schism/markov.js
@@ -13,36 +13,55 @@ var corpusUtils
  * 
  * @param {string} corpus - big body of text to make a new, similar-sounding sentence from
  * @param {number} ?outputSize=20 - number of words to generate
- * @param {number} ?stateSize=2
+ * @param {number} ?stateSize=3
  * @return {string} new sentence based from the corpus
  */
-function _markovChain(corpus, outputSize=20, stateSize=2) {
+async function _markovChain(corpus, outputSize=20, stateSize=3) {
 	if (!corpus) {
-		throw "_markovChain(): did not receive a corpus."
+		throw "_markovChain(): did not receive a corpus"
 	}
 
-	corpus = corpus.split("\n")
+	const cache = Object.create(null) // An object without its usual methods (e.g. .hasOwnProperty())
+	const words = corpus.split(/\s/)
+	const startWords = [words[0]]
+	let next
 
-	const markov = new Markov(corpus, { stateSize })
-	markov.buildCorpus()
-
-	const options = { maxTries: 100 }
-
-	let output = ""
-
-	while (true) {
-		output += markov.generate(options).string
-
-		if (output.length === outputSize) {
-			console.debug("=== path. typeof output:", typeof output)
-			return output
-		} else if (output.length > outputSize) {
-			console.debug("> path. typeof output:", typeof output)
-			return output.split(" ").slice(0, outputSize).join(" ")
-		}
-
-		output += " "
+	/**
+	 * Choose a random entry from an array
+	 * 
+	 * @param {Array} array
+	 * @return random element from an array
+	 */
+	function _choose(arr) {
+		return arr[~~(Math.random() * arr.length)] // ~~() is a "faster substitute for Math.floor()": https://stackoverflow.com/a/5971668
 	}
+
+	/**
+	 * Get the next set of words as a string
+	 */
+	function _nextSet(i) {
+		return words.slice(i, i + stateSize).join(" ")
+	}
+
+	/**
+	 * Analyze corpus
+	 */
+	words.forEach( function(word, i) { // node.js refuses to .bind() with arrow function syntax
+		next = _nextSet(++i)
+		;(cache[word] = cache[word] || []).push(next) // node.js explodes without semicolons here
+		;/[A-Z]/.test(word[0]) && startWords.push(word) // do startWords.push(word) if regex passes
+	}.bind(this))
+
+	/**
+	 * Generate a sentence
+	 */
+	let seed, arr, choice, curr, i = 1
+	arr = [seed = _choose(startWords)]
+	for (; i < outputSize; i += stateSize) { // use semicolon to skip defining the iterator (already defined above)
+		arr.push(choice = _choose(curr || cache[seed])) // cache[seed] if curr is undefined
+		curr = cache[choice.split(" ").pop()]
+	}
+	return arr.join(" ")
 }
 
 
@@ -56,7 +75,7 @@ async function generateSentence(userID) {
 	const corpus = await corpusUtils.load(userID)
 	const wordCount = ~~(Math.random() * 49 + 1) // 1-50 words
 
-	let sentence = _markovChain(corpus, wordCount)
+	let sentence = await _markovChain(corpus, wordCount)
 	return sentence
 }
 
