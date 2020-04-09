a4eab8f512620b4dbb02c2bbc48eb480ac18445f
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-04-09 14:34:02 -0500

    Switch to markov-strings
    markov.js now uses the NPM package markov-strings for its Markov chain algorithm instead of the random one I found in a GitHub Gist one day. Sentences Schism generated in testing are markedly more coherent with the new algorithm than with the old one.
    
    disablePings() was moved somewhere else, and as a result this module no longer needs a DiscordClient object. The function this module exports has been changed to not take one in anymore.
    
    Default state size has been changed from 3 to 2. 2 seems to coherent enough.

--- schism/markov.js
+++ schism/markov.js
@@ -1,5 +1,5 @@
+const Markov = require("markov-strings").default
 var corpusUtils
-var client
 
 
 /**
@@ -12,77 +12,37 @@ var client
  *   throwing it away to make a new one with different data.
  * 
  * @param {string} corpus - big body of text to make a new, similar-sounding sentence from
- * @param {number} [outputSize=20] - number of words to generate (default: 20)
- * @param {number} [stateSize=3] - chain order (default: 3)
- * @return {Promise<string>} new sentence based from the corpus
+ * @param {number} ?outputSize=20 - number of words to generate
+ * @param {number} ?stateSize=2
+ * @return {string} new sentence based from the corpus
  */
-async function _markovChain(corpus, outputSize=20, stateSize=3) {
+function _markovChain(corpus, outputSize=20, stateSize=2) {
 	if (!corpus) {
-		throw "_markovChain(): did not receive a corpus"
+		throw "_markovChain(): did not receive a corpus."
 	}
 
-	const cache = Object.create(null) // An object without its usual methods (e.g. .hasOwnProperty())
-	const words = corpus.split(/\s/)
-	const startWords = [words[0]]
-	let next
+	corpus = corpus.split("\n")
 
-	/**
-	 * Choose a random entry from an array
-	 * 
-	 * @param {Array} array
-	 * @return random element from an array
-	 */
-	function _choose(arr) {
-		return arr[~~(Math.random() * arr.length)] // ~~() is a "faster substitute for Math.floor()": https://stackoverflow.com/a/5971668
+	const markov = new Markov(corpus, { stateSize })
+	markov.buildCorpus()
+
+	const options = { maxTries: 100 }
+
+	let output = ""
+
+	while (true) {
+		output += markov.generate(options).string
+
+		if (output.length === outputSize) {
+			console.debug("=== path. typeof output:", typeof output)
+			return output
+		} else if (output.length > outputSize) {
+			console.debug("> path. typeof output:", typeof output)
+			return output.split(" ").slice(0, outputSize).join(" ")
+		}
+
+		output += " "
 	}
-
-	/**
-	 * Get the next set of words as a string
-	 */
-	function _nextSet(i) {
-		return words.slice(i, i + stateSize).join(" ")
-	}
-
-	/**
-	 * Analyze corpus
-	 */
-	words.forEach( function(word, i) { // node.js refuses to .bind() with arrow function syntax
-		next = _nextSet(++i)
-		;(cache[word] = cache[word] || []).push(next) // node.js explodes without semicolons here
-		;/[A-Z]/.test(word[0]) && startWords.push(word) // do startWords.push(word) if regex passes
-	}.bind(this))
-
-	/**
-	 * Generate a sentence
-	 */
-	let seed, arr, choice, curr, i = 1
-	arr = [seed = _choose(startWords)]
-	for (; i < outputSize; i += stateSize) { // use semicolon to skip defining the iterator (already defined above)
-		arr.push(choice = _choose(curr || cache[seed])) // cache[seed] if curr is undefined
-		curr = cache[choice.split(" ").pop()]
-	}
-	return arr.join(" ")
-}
-
-
-/**
- * An asynchronous version of String.prototype.replace().
- * Created by Overcl9ck on StackOverflow:
- * https://stackoverflow.com/a/48032528
- * 
- * @param {string} str - string to run the function on
- * @param {RegExp} regex - match to this regular expression
- * @param {Function} asyncFn - function to be invoked to replace the matches to [regex]
- * @return {string} string with matches to [regex] processed by [asyncFn]
- */
-async function _replaceAsync(str, regex, asyncFn) {
-    const promises = []
-    str.replace(regex, (match, ...args) => {
-        const promise = asyncFn(match, ...args)
-        promises.push(promise)
-    })
-    const data = await Promise.all(promises)
-    return str.replace(regex, () => data.shift())
 }
 
 
@@ -96,23 +56,16 @@ async function generateSentence(userID) {
 	const corpus = await corpusUtils.load(userID)
 	const wordCount = ~~(Math.random() * 49 + 1) // 1-50 words
 
-	let sentence = await _markovChain(corpus, wordCount, 3)
-	sentence = sentence.substring(0, 256) // Hard cap of 256 characters (any longer is just too big)
+	let sentence = _markovChain(corpus, wordCount)
 	return sentence
 }
 
 
 /**
- * @param {Client} input_client - A Discord client for markov.js to use
  * @param {Object} input_corpusUtils - An instance of corpus.js
  */
-module.exports = (input_client, input_corpusUtils) => {
-	client = input_client
+module.exports = (input_corpusUtils) => {
 	corpusUtils = input_corpusUtils
-
-	if (!client) {
-		throw "Missing argument: no client provided"
-	}
 
 	if (!corpusUtils) {
 		throw "Missing argument: no corpus.js provided"
