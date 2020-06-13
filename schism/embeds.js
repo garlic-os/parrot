d1122fe7c54683de8ee35d9eb745080dc55539af
garlicOS® <14845347+the-garlic-os@users.noreply.github.com>
2020-06-13 02:15:03 -0500

    New EULA- and forget-related embeds
    - "consent" appears when a user tries to make Schism imitate themself when they have not agreed to the EULA.
    - "agree" appears when a user agrees to the EULA.
    - I bet you can't guess what "disagree" does.
    - "confirmForget" appears when a user requests to delete their data. Upon Schism's receiving such a request, the user will have a limited amount of time to confirm their action with a second command. This action is permanent, so this "double-check" system is in place to keep it from happening on accident.
    - "confirmationExpired" appears when a user tries to confirm a request to delete their data when the confirmation window has passed.
    - "forgot" appears when Schism succeeds in erasing a user's data from itself and its servers.
    - "eula" prints the End User License Agreement from /eula.txt

--- schism/embeds.js
+++ schism/embeds.js
@@ -1,17 +1,85 @@
+const fs = require("fs")
 const { RichEmbed } = require("discord.js")
-const colors = require("./config").EMBED_COLORS
-
+const config = require("./config")
+const colors = config.EMBED_COLORS
+const prefix = config.PREFIX
 
 /**
  * Generate a Discord Rich Embed message.
  * 
  * @param {string} str - message to print
+ * @param {DiscordUser} author? - (optional) author to assign to the embed
  * @return {RichEmbed} Discord Rich Embed object
  */
-function standard(str) {
+function standard(str, author) {
+	const embed = new RichEmbed()
+		.setColor(colors.normal)
+		.setDescription(str)
+
+	if (author) {
+		embed.setAuthor(author.tag, author.avatarURL)
+	}
+
+	return embed
+}
+
+
+function consent(user) {
 	return new RichEmbed()
 		.setColor(colors.normal)
-		.addField("Schism", str)
+		.setAuthor(user.tag, user.avatarURL)
+		.setTitle(`${user.tag}, did you agree to Schism's End User License Agreement?`)
+		.setDescription(`View the full End User License Agreement with \`${prefix}eula\`, then make your decision with \`${prefix}agree\` or \`${prefix}disagree\`.`)
+		.setFooter("Schism keeps your messages to gain an impression of how you speak. You must agree to the End User License Agreement before Schism can imitate you.")
+}
+
+
+function agree(user) {
+	return new RichEmbed()
+		.setColor(colors.normal)
+		.setAuthor(user.tag, user.avatarURL)
+		.setDescription("You have agreed to Schism's End User License Agreement. You can now make Schism imitate you!")
+		.setFooter(`View the full EULA: ${prefix}eula • Revoke consent: ${prefix}disagree`)
+}
+
+
+function disagree(user) {
+	return new RichEmbed()
+		.setColor(colors.normal)
+		.setAuthor(user.tag, user.avatarURL)
+		.setDescription("You have disagreed to Schism's End User License Agreement. Schism will no longer be able to imitate you.")
+		.setFooter(`View the full EULA: ${prefix}eula • Restore consent: ${prefix}agree`)
+}
+
+
+function confirmForget(caller, forgetee) {
+	const self = caller.id === forgetee.id
+	return new RichEmbed()
+		.setColor(colors.normal)
+		.setAuthor(caller.tag, caller.avatarURL)
+		.setTitle(`Are you sure you want Schism to forget ${self ? "you" : `**${forgetee.tag}**`}?`)
+		.setDescription(`To confirm, do \`${prefix}confirmforget${self ? "" : ` @${forgetee.username}`}\` within the next 30 seconds.`)
+		.setFooter(`${self ? "Your" : "This user's"} data will be permanently erased from Schism's servers.`)
+
+}
+
+
+function confirmationExpired(caller, forgetee) {
+	const self = caller.id === forgetee.id
+	return new RichEmbed()
+		.setColor(colors.normal)
+		.setAuthor(caller.tag, caller.avatarURL)
+		.setTitle("No pending confirmation")
+		.setDescription(`There is no pending confirmation for you to delete ${self ? "your" : `**${forgetee.tag}**'s`} data. This means that either the confirmation window has expired, or that you have never requested for Schism to forget ${self ? "you" : `**${forgetee.tag}**`}.`)
+}
+
+
+function forgot(caller, forgetee) {
+	return new RichEmbed()
+		.setColor(colors.normal)
+		.setAuthor(caller.tag, caller.avatarURL)
+		.setTitle(`Forgot **${forgetee.tag}**.`)
+		.setDescription(`This user's data has been permanently erased from Schism's servers.`)
 }
 
 
@@ -24,7 +92,7 @@ function standard(str) {
 function error(err) {
 	return new RichEmbed()
 		.setColor(colors.error)
-		.addField("Error", err)
+		.setDescription(err)
 }
 
 
@@ -45,13 +113,28 @@ const xok = new RichEmbed()
  */
 const code = new RichEmbed()
 	.setTitle("Schism is open source!")
-	.addField("View the code, file issues, and make pull requests to help improve Schism.", "https://github.com/the-garlic-os/schism")
+	.addField("View the code, file issues, and make pull requests to help improve Schism.", "https://github.com/Grosserly/schism")
 	.setFooter("Please help me. I'm begging you.")
+
+
+/**
+ * A pre-made embed containing Schism's End User License Agreement.
+ */
+const eula = new RichEmbed()
+	.setTitle("Schism's End User License Agreement")
+	.setDescription(fs.readFileSync("./eula.txt")) // Yes, this will break if it pushes the embed over 6,000 characters
 
 
 module.exports = {
 	standard,
+	consent,
+	agree,
+	disagree,
+	forgot,
+	confirmForget,
+	confirmationExpired,
 	error,
 	xok,
-	code
+	code,
+	eula
 }
