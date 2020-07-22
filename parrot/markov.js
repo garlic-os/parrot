12c847b937d32f3e790bfec3a4ac6eb43f5e8063
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-07-22 00:18:34 -0500

    New markov algorithm
    Changing from a GitHub Gist I stumbled upon (thanks, @kevincennis!) to purpl-markov-chain for Markov-chain-based sentence generation. This will open the door to some speed optimizations and security enhancements I have planned further down the road.

--- parrot/markov.js
+++ parrot/markov.js
@@ -1,67 +1,5 @@
 var corpusUtils;
-
-
-/**
- * Quick and dirty Markov chain text generator
- * By kevincennis on GitHub
- * https://gist.github.com/kevincennis/5440878
- * 
- * I have modified this algorithm to be better for one-time use,
- *   since Parrot will only use a Markov once before
- *   throwing it away to make a new one with different data.
- * 
- * @param {string} corpus - big body of text to make a new, similar-sounding sentence from
- * @param {number} ?outputSize=20 - number of words to generate
- * @param {number} ?stateSize=3
- * @return {string} new sentence based from the corpus
- */
-async function _markovChain(corpus, outputSize=20, stateSize=3) {
-	if (!corpus) {
-		throw "_markovChain(): did not receive a corpus";
-	}
-
-	const cache = Object.create(null); // An object without its usual methods (e.g. .hasOwnProperty())
-	const words = corpus.split(/\s/);
-	const startWords = [words[0]];
-	let next;
-
-	/**
-	 * Choose a random entry from an array
-	 * 
-	 * @param {Array} array
-	 * @return random element from an array
-	 */
-	function _choose(arr) {
-		return arr[~~(Math.random() * arr.length)]; // ~~() is a "faster substitute for Math.floor()": https://stackoverflow.com/a/5971668
-	}
-
-	/**
-	 * Get the next set of words as a string
-	 */
-	function _nextSet(i) {
-		return words.slice(i, i + stateSize).join(" ");
-	}
-
-	/**
-	 * Analyze corpus
-	 */
-	words.forEach( function(word, i) { // node.js refuses to .bind() with arrow function syntax
-		next = _nextSet(++i);
-		(cache[word] = cache[word] || []).push(next);
-		/[A-Z]/.test(word[0]) && startWords.push(word); // do startWords.push(word) if regex passes
-	}.bind(this));
-
-	/**
-	 * Generate a sentence
-	 */
-	let seed, arr, choice, curr, i = 1;
-	arr = [seed = _choose(startWords)]
-	for (; i < outputSize; i += stateSize) { // use semicolon to skip defining the iterator (already defined above)
-		arr.push(choice = _choose(curr || cache[seed])); // cache[seed] if curr is undefined
-		curr = cache[choice.split(" ").pop()];
-	}
-	return arr.join(" ");
-}
+const MarkovChain = require("purpl-markov-chain");
 
 
 /**
@@ -72,10 +10,21 @@ async function _markovChain(corpus, outputSize=20, stateSize=3) {
  */
 async function generateSentence(userID) {
 	const corpus = await corpusUtils.load(userID);
-	const wordCount = ~~(Math.random() * 49 + 1); // 1-50 words
+	const sentenceCount = ~~(Math.random() * 4 + 1); // 1-5 sentences
+	const chain = new MarkovChain();
+	chain.config.grams = ~~(Math.random() * 2 + 1); // State size 1-3
 
-	let sentence = await _markovChain(corpus, wordCount);
-	return sentence;
+	for (const line of corpus.split("\n")) {
+		chain.update(line);
+	}
+
+	let phrase = "";
+
+	for (let i = 0; i < sentenceCount; ++i) {
+		phrase += chain.generate();
+	}
+
+	return phrase;
 }
 
 
