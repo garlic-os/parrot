09f5313bb1881ad55811051a49a0e92fae39e884
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-12-12 22:02:35 -0600

    Fix and comment Userlike
    Userlike wasn't working because Guild.get_members() inexplicably isn't working no matter what I try. So I refactored it to use Guild.fetch_members(), instead, which the docs say you shouldn't do. Well, docs, I wouldn't do it if the thing you recommended actually worked!
    
    Userlike now works properly in DMChannels. Before, it didn't work because it tried to call a method on ctx.guild, which doesn't exist in DMChannels.
    
    Now, if you pass in something that doesn't parse to an int, it will just say the user isn't found instead of throwing a spooky ValueError.
    
    I also changed the indentation in the bulleted list in the docstring to use four spaces so that the indentation width doesn't get screwed up throughout in the file in VS Code (and probably other IDEs).

--- utils/converters.py
+++ utils/converters.py
@@ -2,20 +2,39 @@ import re
 from discord.ext import commands
 from discord import User
 
+from discord.errors import NotFound
+from utils.exceptions import UserNotFoundError
+
 
 class Userlike(commands.Converter):
     """
     A string that can resolve to a User.
     Works with:
-      - Mentions, like <@394750023975409309> and <@!394750023975409309>
-      - User IDs, like 394750023975409309
-      - The string "me" or "myself", which resolves to the context's author
+        - Mentions, like <@394750023975409309> and <@!394750023975409309>
+        - User IDs, like 394750023975409309
+        - The string "me" or "myself", which resolves to the context's author
     """
 
     async def convert(self, ctx: commands.Context, text: str) -> User:
         if text in ("me", "myself"):
             return ctx.author
 
-        # Remove all non-numeric characters
-        user_id = int(re.sub("[^0-9]", "", text))
-        return await ctx.guild.get_member(user_id)
+        # Use this error if anything goes wrong.
+        user_not_found = UserNotFoundError(f"User \"{text}\" does not exist.")
+
+        # If this is not a guild, it must be a DM channel, and therefore the
+        #   only person you can imitate is yourself.
+        if ctx.guild is None:
+            raise user_not_found
+
+        # Strip the mention down to an ID.
+        try:
+            user_id = int(re.sub("[^0-9]", "", text))
+        except ValueError:
+            raise user_not_found
+
+        # Fetch the member by ID.
+        try:
+            return await ctx.guild.fetch_member(user_id)
+        except NotFound:
+            raise user_not_found
