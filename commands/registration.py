ac51c4f068ad8ad1f924f02edabbe4df41bb4fa0
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-12-14 22:13:38 -0600

    Rename RegistrationCommands category to Registration

--- commands/registration.py
+++ commands/registration.py
@@ -1,10 +1,39 @@
 from discord.ext import commands
-from utils.pembed import Pembed
+from utils.parrot_embed import ParrotEmbed
 
 
-class RegistrationCommands(commands.Cog):
+class Registration(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        with open("privacy-policy.txt", "r") as f:
+            self.policy_text = f.read()
+
+
+    @commands.command(
+        aliases=[
+            "privacy_policy",
+            "privacypolicy",
+            "privacy",
+            "terms_of_service",
+            "termsofservice",
+            "tos",
+            "terms",
+            "eula",
+        ],
+        brief="View Parrot's privacy policy.",
+    )
+    @commands.cooldown(2, 4, commands.BucketType.user)
+    async def policy(self, ctx: commands.Context) -> None:
+        """
+        View Parrot's privacy policy.
+        Parrot collects the message history of registered users to imitate them. Learn more about Parrot's data collection practices here.
+        """
+        embed = ParrotEmbed(title="Privacy Policy", description=self.policy_text)
+        embed.set_footer(
+            text=f"{self.bot.command_prefix}register • {self.bot.command_prefix}unregister"
+        )
+        await ctx.send(embed=embed)
+
 
     @commands.command(brief="Register with Parrot.")
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -15,20 +44,19 @@ class RegistrationCommands(commands.Cog):
         """
         self.bot.registration.add(ctx.author.id)
 
-        embed = Pembed(
+        embed = ParrotEmbed(
             title="✅ Registered!",
             color_name="green",
             description="Now Parrot can start learning your speech patterns and imitate you.",
         )
         embed.add_field(
             name="Tip:",
-            value="If this is your first time registering (or if you deleted your data recently), you might want to consider running the `quickstart` command to immediately give Parrot a dataset to imitate you from. This will scan your past messages to create a model of how you speak, so you can start using Parrot right away.",
+            value=f"If this is your first time registering (or if you deleted your data recently), you might want to consider running the `{self.bot.command_prefix}quickstart` command to immediately give Parrot a dataset to imitate you from. This will scan your past messages to create a model of how you speak, so you can start using Parrot right away.",
         )
-        embed.set_footer(text=f"{self.bot.command_prefix}quickstart")
 
         await ctx.send(embed=embed)
 
-    @commands.command(brief="Unregister from Parrot.")
+    @commands.command(brief="Unregister with Parrot.")
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def unregister(self, ctx: commands.Context) -> None:
         """
@@ -37,7 +65,7 @@ class RegistrationCommands(commands.Cog):
         """
         self.bot.registration.remove(ctx.author.id)
 
-        embed = Pembed(
+        embed = ParrotEmbed(
             title="Unregistered!",
             color_name="gray",
             description=f"Parrot will no longer be able to imitate you, and it has stopped collecting your messages.\n\n_If you're done with Parrot and don't want it to have your messages anymore, or if you just want a fresh start, you can do `{self.bot.command_prefix}forget me` and your existing data will be permanently deleted from Parrot._",
@@ -47,16 +75,16 @@ class RegistrationCommands(commands.Cog):
 
     @commands.command(
         aliases=[
+            "registration_status",
             "registrationstatus",
             "registration",
-            "status",
             "checkregistration",
             "check_registration",
         ],
         brief="Check if you're registered with Parrot.",
     )
     @commands.cooldown(1, 2, commands.BucketType.user)
-    async def registration_status(self, ctx: commands.Context) -> None:
+    async def status(self, ctx: commands.Context) -> None:
         """
         Check if you're registered with Parrot.
         You need to be registered for Parrot to be able to analyze your messages and imitate you.
@@ -68,4 +96,4 @@ class RegistrationCommands(commands.Cog):
 
 
 def setup(bot: commands.Bot) -> None:
-    bot.add_cog(RegistrationCommands(bot))
+    bot.add_cog(Registration(bot))
