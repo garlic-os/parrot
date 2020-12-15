f07389b17e2cd8ef365dc0c8dcd7faa6ac6582e3
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-12-14 22:23:08 -0600

    Add types for "forget" confirmations
    Types ConfirmationBody and PendingConfirmations have been created to model the data structure that the "forget" command uses to keep track of who is still on the command's "Are You Sure?" menu.

--- utils/types.py
+++ utils/types.py
@@ -1,9 +1,24 @@
 from typing import Dict, NamedTuple
+from discord import User
 
 
 class CorpusMessage(NamedTuple):
+    # Discord.Message.content
     content: str
+
+    # str(Discord.Message.created_at)
     timestamp: str
 
+# Key: Discord.Message.id
+Corpus = Dict[int, CorpusMessage]
 
-Corpus = Dict[str, CorpusMessage]
+
+class ConfirmationBody(NamedTuple):
+    # The forget command message's author
+    author: User
+
+    # User for whose the corpus to be forgotten
+    corpus_owner: User
+
+# Key: Message ID of a forget command
+PendingConfirmations = Dict[int, ConfirmationBody]
