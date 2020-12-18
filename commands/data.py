5b544f7e0b2a02c206a3344f3020aa5745bd9f14
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-12-18 01:59:05 -0600

    No more forget cancel
    "|forget cancel confirmation_id" is gone! Just let it expire on its own. The confirmation embed itself has been updated to reflect this.

--- commands/data.py
+++ commands/data.py
@@ -10,8 +10,7 @@ from utils.converters import Userlike
 
 
 class Data(commands.Cog):
-    def __init__(self, bot: commands.Bot) -> None:
-        self.bot = bot
+    def __init__(self) -> None:
         self.pending_confirmations: PendingConfirmations = {}
 
 
@@ -22,7 +21,7 @@ class Data(commands.Cog):
         user = ctx.author
 
         try:
-            corpus_file_path = self.bot.corpora.file_path(user)
+            corpus_file_path = ctx.bot.corpora.file_path(user)
         except FileNotFoundError:
             raise NoDataError(f"No data available for user {user.name}#{user.discriminator}.")
 
@@ -51,7 +50,7 @@ class Data(commands.Cog):
 
 
     @commands.group()
-    @commands.cooldown(2, 5, commands.BucketType.user)
+    @commands.cooldown(2, 4, commands.BucketType.user)
     async def forget(self, ctx: commands.Context, maybe_user: str, *args) -> None:
         """ Make Parrot delete all the data it has about you. """
         try:
@@ -64,17 +63,17 @@ class Data(commands.Cog):
                     return
             raise
 
-        if user != ctx.author and ctx.author.id not in self.bot.owner_ids:
+        if user != ctx.author and ctx.author.id not in ctx.bot.owner_ids:
             raise UserPermissionError("You are not allowed to make Parrot forget other users.")
 
-        if user not in self.bot.corpora:
+        if user not in ctx.bot.corpora:
             raise NoDataError(f"No data available for user {user.name}#{user.discriminator}.")
 
-        confirmation_id = ctx.message.id
+        confirm_code = ctx.message.id
 
         # Keep track of this confirmation by storing some information about it
         #   in a dict.
-        self.pending_confirmations[confirmation_id] = {
+        self.pending_confirmations[confirm_code] = {
             "author": ctx.author,
             "corpus_owner": user,
         }
@@ -82,35 +81,28 @@ class Data(commands.Cog):
         embed = ParrotEmbed(
             author=user,
             title="Are you sure?",
-            description=f"This will permantently delete the data of {user.name}#{user.discriminator}.\nTo make your choice, paste one of these two commands:",
+            description=f"This will permantently delete the data of {user.name}#{user.discriminator}.\nTo confirm, paste the following command:\n`{ctx.bot.command_prefix}forget confirm {confirm_code}`",
             color_name="orange",
         )
-        embed.add_field(
-            name="✅ Confirm",
-            value=f"{self.bot.command_prefix}forget confirm {confirmation_id}",
-        )
-        embed.add_field(
-            name="❌ Cancel",
-            value=f"{self.bot.command_prefix}forget cancel {confirmation_id}",
-        )
-        embed.set_footer(text="Confirmation will be automatically canceled in 1 minute.")
+        embed.set_footer(text="Action will be automatically canceled in 1 minute.")
 
         await ctx.send(embed=embed)
 
         # Delete the confirmation after 1 minute.
         await asyncio.sleep(60)
         try:
-            del self.pending_confirmations[confirmation_id]
+            del self.pending_confirmations[confirm_code]
         except KeyError:
             pass
 
 
     @forget.command(name="confirm", hidden=True)
-    async def forget_confirm(self, ctx: commands.Context, confirmation_id: int) -> None:
-        # You'd think that since this argument is typed as an int, it would be
-        #   an int. But noooo, it's actually a string.
-        confirmation_id = int(confirmation_id)
-        confirmation = self.pending_confirmations.get(confirmation_id, None)
+    @commands.cooldown(2, 4, commands.BucketType.user)
+    async def forget_confirm(self, ctx: commands.Context, confirm_code: int) -> None:
+        # You'd think that since this argument is typed as an int, it would come
+        #   in as an int. But noooo, it's actually a string.
+        confirm_code = int(confirm_code)
+        confirmation = self.pending_confirmations.get(confirm_code, None)
         
         confirmation_is_valid = (
             confirmation is not None and
@@ -119,36 +111,19 @@ class Data(commands.Cog):
 
         if confirmation_is_valid:
             user = confirmation["corpus_owner"]
-            del self.bot.corpora[user]
-            del self.bot.chains[user]
-            del self.pending_confirmations[confirmation_id]
+            del ctx.bot.corpora[user]
+            del ctx.bot.chains[user]
+            del self.pending_confirmations[confirm_code]
 
             await ctx.send(embed=ParrotEmbed(
                 author=user,
                 title=f"Parrot has forgotten {user.name}#{user.discriminator}.",
                 description="All of the data that Parrot has collected from this user has been deleted.",
-                color_nane="gray",
+                color_name="gray",
             ))
         else:
-            await ctx.send(f"Confirmation code \"{confirmation_id}\" is invalid.")
-
-
-    @forget.command(name="cancel", hidden=True)
-    async def forget_cancel(self, ctx: commands.Context, confirmation_id: int) -> None:
-        confirmation_id = int(confirmation_id)
-        confirmation = self.pending_confirmations.get(confirmation_id, None)
-        
-        confirmation_is_valid = (
-            confirmation is not None and
-            confirmation["author"] == ctx.author
-        )
-
-        if confirmation_is_valid:
-            del self.pending_confirmations[confirmation_id]
-            await ctx.send("Forget command canceled.")
-        else:
-            await ctx.send(f"Confirmation code \"{confirmation_id}\" is invalid.")
+            await ctx.send(f"Confirmation code `{confirm_code}` is invalid.")
 
 
 def setup(bot: commands.Bot) -> None:
-    bot.add_cog(Data(bot))
+    bot.add_cog(Data())
