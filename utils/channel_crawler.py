e737d8b421a2946127ec517c3193216863910288
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-12-18 01:45:41 -0600

    Create channel_crawler.py
    This is my alternative solution to the auto-updating embed for Quickstart. I can asyncio.gather ChannelCrawler.crawl along with a method that updates Quickstart's status embed on a timer, and they'll both run in harmony with each other.

---
+++ utils/channel_crawler.py
@@ -0,0 +1,30 @@
+from typing import Callable
+from discord import Message
+from discord.iterators import _FilteredAsyncIterator
+from utils.types import Channel
+
+
+class ChannelCrawler:
+    def __init__(self, history: _FilteredAsyncIterator, action: Callable[[Message], bool]) -> None:
+        self.num_collected = 0
+        self.running = True
+        self._action = action
+        self._history = history
+
+    async def crawl(self) -> None:
+        """
+        Iterate over up to [limit] messages in the channel in
+        reverse-chronological order.
+        """
+        async for message in self._history:
+            if not self.running:
+                break
+
+            if self._action(message):
+                self.num_collected += 1
+
+        self.running = False
+
+
+    def stop(self) -> None:
+        self.running = False
