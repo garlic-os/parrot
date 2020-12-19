1603cd2214689f34fd4a63b4ed079851feba0760
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-12-18 22:35:49 -0600

    Let admins do Quickstart on bots
    This change pretty much just required a little new logic and a bunch of changes to the embed messages.

--- commands/quickstart.py
+++ commands/quickstart.py
@@ -1,11 +1,14 @@
 from typing import Dict, Callable, List
-from discord import ChannelType, Embed, Message, TextChannel
+from discord import ChannelType, Embed, Message, TextChannel, User
 
 import asyncio
 from discord.ext import commands
 from utils.parrot_embed import ParrotEmbed
 from utils.channel_crawler import ChannelCrawler
-from utils.exceptions import AlreadyScanning
+from utils.exceptions import AlreadyScanning, UserPermissionError
+from utils.checks import is_admin
+from utils.converters import Userlike
+from utils import Paginator
 
 
 class Quickstart(commands.Cog):
@@ -17,7 +20,7 @@ class Quickstart(commands.Cog):
         self.ongoing_scans: Dict[int, List[int]] = {}
 
 
-    async def live_update_status(self, source_channel: TextChannel, status_message: Message, crawler: ChannelCrawler) -> None:
+    async def live_update_status(self, source_channel: TextChannel, status_message: Message, user: User, crawler: ChannelCrawler) -> None:
         while crawler.running:
             embed = ParrotEmbed(
                 description=f"**Scanning {source_channel.mention}...**\nCollected {crawler.num_collected} new messages...",
@@ -26,28 +29,42 @@ class Quickstart(commands.Cog):
                 name="Quickstart",
                 icon_url="https://i.gifer.com/ZZ5H.gif",  # Loading spinner
             )
+            embed.set_footer(
+                text=f"Scanning for {user.name}#{user.discriminator}",
+                icon_url=user.avatar_url,
+            )
             await status_message.edit(embed=embed)
             await asyncio.sleep(2)
 
 
     @commands.command()
     @commands.cooldown(2, 4, commands.BucketType.user)
-    async def quickstart(self, ctx: commands.Context) -> None:
+    async def quickstart(self, ctx: commands.Context, user: Userlike=None) -> None:
         """ Scan your past messages to get started using Parrot right away. """
+        if user is None or ctx.author == user:
+            user = ctx.author
+        else:
+            if not is_admin(ctx):
+                raise UserPermissionError("You can only run Quickstart on yourself.")
+            if not user.bot:
+                raise UserPermissionError("Quickstart can only be run on behalf of bots.")
+
         if ctx.channel.id not in self.ongoing_scans:
             self.ongoing_scans[ctx.channel.id] = []
 
         # You can't run Quickstart in a channel that Quickstart is already
         #   currently scanning for you.
-        if ctx.author.id in self.ongoing_scans[ctx.channel.id]:
-            raise AlreadyScanning("❌ You are already currently running Quickstart in this channel!")
+        if user.id in self.ongoing_scans[ctx.channel.id]:
+            if ctx.author == user:
+                raise AlreadyScanning("❌ You are already currently running Quickstart in this channel!")
+            raise AlreadyScanning(f"❌ Quickstart is already running for {user.name}#{user.discriminator} in this channel!")
 
         # Record that Quickstart is scanning this channel for this user.
-        self.ongoing_scans[ctx.channel.id].append(ctx.author.id)
+        self.ongoing_scans[ctx.channel.id].append(user.id)
 
         # Tell the user off if they try to run this command in a DM channel.
         if ctx.channel.type != ChannelType.text:
-            await ctx.send(f"Quickstart is only available in servers. Try running \`{ctx.bot.command_prefix}quickstart\` again in a server that Parrot is in.")
+            await ctx.send(f"Quickstart is only available in servers. Try running Quickstart again in a server that Parrot is in.")
             return
 
         # Show the user where they can use Quickstart within this server if they
@@ -55,7 +72,7 @@ class Quickstart(commands.Cog):
         if ctx.channel.id not in ctx.bot.learning_channels:
             embed = ParrotEmbed(
                 title="Quickstart Channels",
-                description=f"Quickstart is available in channels where Parrot can learn from your messages. Try running \`{ctx.bot.command_prefix}quickstart\` again in one of these channels:",
+                description="Quickstart is available in channels where Parrot can learn from your messages. Try running Quickstart again in one of these channels:",
             )
             channel_mentions = []
             for channel in ctx.guild.channels:
@@ -70,10 +87,14 @@ class Quickstart(commands.Cog):
             await paginator.run()
             return
 
-        corpus = ctx.bot.corpora.get(ctx.author, {})
+        corpus = ctx.bot.corpora.get(user, {})
     
         # Create and embed that will show the status of the Quickstart
         #   operation and DM it to the user who invoked the command.
+        if ctx.author == user:
+            name = "your"
+        else:
+            name = f"{user.mention}'s"
         embed = ParrotEmbed(
             description=f"**Scanning {ctx.channel.mention}...**\nCollected 0 new messages...",
         )
@@ -81,19 +102,23 @@ class Quickstart(commands.Cog):
             name="Quickstart",
             icon_url="https://i.gifer.com/ZZ5H.gif",  # Loading spinner
         )
+        embed.set_footer(
+            text=f"Scanning for {user.mention}",
+            icon_url=user.avatar_url,
+        )
         status_message = await ctx.author.send(embed=embed)
         await ctx.send(embed=ParrotEmbed(
             title="Quickstart is scanning",
-            author=ctx.author,
-            description="Parrot is now scanning this channel and learning from your past messages.\nThis could take a few minutes.\nCheck your DMs to see its progress."
+            author=user,
+            description=f"Parrot is now scanning this channel and learning from {name} past messages.\nThis could take a few minutes.\nCheck your DMs to see its progress."
         ))
 
         # Create an iterator representing this channel's past messages.
         def message_filter(message: Message) -> bool:
-            return message.author == ctx.author
+            return message.author == user
         history = ctx.channel.history(
             limit=10_000,
-            after=ctx.author.joined_at,
+            after=user.joined_at,
         ).filter(message_filter)
 
         # Create an object that will scan through the channel's message history
@@ -112,25 +137,34 @@ class Quickstart(commands.Cog):
         #   running) to tell it to stop.
         await asyncio.gather(
             crawler.crawl(),
-            self.live_update_status(ctx.channel, status_message, crawler),
+            self.live_update_status(
+                source_channel=ctx.channel,
+                status_message=status_message,
+                user=user,
+                crawler=crawler,
+            ),
             loop=ctx.bot.loop,    
         )
 
         # Update the status embed one last time, but DELETE it this time and
         #   post a brand new one so that the user gets a new notification.
+        if ctx.author == user:
+            name = "you"
+        else:
+            name = f"{user.name}#{user.discriminator}"
         embed = ParrotEmbed(
             description=f"**Scan in {ctx.channel.mention} complete.**\nCollected {crawler.num_collected} new messages.",
         )
         embed.set_author(name="✅ Quickstart")
         if crawler.num_collected == 0:
-            embed.description = "😕 Couldn't find any messages from you in this channel.\n" + embed.description
+            embed.description += f"\n😕 Couldn't find any messages from {name} in this channel."
             embed.color = ParrotEmbed.colors["red"]
         await asyncio.gather(
             status_message.delete(),
             ctx.author.send(embed=embed)
         )
 
-        self.ongoing_scans[ctx.channel.id].remove(ctx.author.id)
+        self.ongoing_scans[ctx.channel.id].remove(user.id)
         if len(self.ongoing_scans[ctx.channel.id]) == 0:
             del self.ongoing_scans[ctx.channel.id]
 
