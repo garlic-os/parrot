019475d27ebad66d65e176ce3a61036c9f57a072
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-12-19 00:19:47 -0600

    Make ParrotMarkov not copy and paste entire sentences
    I misunderstood how parsed_sentences works. I'm not using it anymore, because the Markov was basically just choosing an entire sentence at random and passing it off as its own without modifying it at all. That won't do. The way that I have set it up now makes it so that it is more creative.

--- utils/parrot_markov.py
+++ utils/parrot_markov.py
@@ -5,38 +5,33 @@ import markovify
 import random
 
 
-class ParrotMarkov(markovify.Text):
+class ParrotMarkov(markovify.NewlineText):
     """
     A subclass of markovify.Text customized for use with Parrot.
     This class takes in a Parrot Corpus instead of a string and sends the
-      sentences within the Corpus straight to the Markov chain's
-      parsed_sentences parameter. This also bypasses the chain's need for
-      its input to be well-punctuated for parsing. Because Heaven knows you're
-      not going to get that when the dataset is coming from Discord.
+    sentences within the Corpus straight to the Markov chain's parsed_sentences
+    parameter. This also disables the chain's need for its input to be
+    well-punctuated for parsing. Because Heaven knows you're not going to get
+    that when the dataset is coming from Discord.
 
     Parrot Corpora also don't have any limit on how large they can get, so I
-      thought it would be a good idea to tell the Markov constructor not to
-      keep its corpus in memory.
+    thought it would be a good idea to tell the constructor not to keep its
+    corpus in memory.
 
     The state size of each instance is now a random choice between 2 and 3.
     """
 
     def __init__(self, corpus: Corpus, state_size: int = 2) -> None:
         super().__init__(
-            None,
-            parsed_sentences=ParrotMarkov.parse_corpus(corpus),
+            self.parse_corpus(corpus),
             state_size=random.randint(2, 3),
             retain_original=False,
+            well_formed=False,
         )
 
-    @staticmethod
-    def parse_corpus(corpus: Corpus) -> List[List[str]]:
-        """
-        Parse a Corpus into a format that markovify likes.
-        Rows are sentences; columns are the words in the sentence.
-        """
-        return [message["content"].split(" ") for message in corpus.values()]
-        # sentences = []
-        # for message in corpus.values():
-        #     sentences.append(message.content.split(" "))
-        # return sentence
+    def parse_corpus(self, corpus: Corpus) -> str:
+        """ Parse a Corpus into one, long, newline-delimited string. """
+        sentences = ""
+        for message in corpus.values():
+            sentences += message["content"] + "\n"
+        return sentences
