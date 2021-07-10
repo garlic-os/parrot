0d28b27981b312b245a55d109ae42965afe8f549
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-07-09 23:52:38 -0500

    Cleanup comments

--- utils/types.py
+++ utils/types.py
@@ -3,22 +3,16 @@ from discord import DMChannel, GroupChannel, TextChannel, User
 
 
 class CorpusMessage(NamedTuple):
-    # Discord.Message.content
-    content: str
-
-    # str(Discord.Message.created_at)
-    timestamp: str
+    content: str  # Discord.Message.content
+    timestamp: float  # Discord.Message.created_at.timestamp()
 
 # Key: Discord.Message.id
 Corpus = Dict[int, CorpusMessage]
 
 
 class ConfirmationBody(NamedTuple):
-    # The forget command message's author
-    author: User
-
-    # User for whose the corpus to be forgotten
-    corpus_owner: User
+    author: User  # The forget command message's author
+    corpus_owner: User  # User for whose the corpus to be forgotten
 
 # Key: Message ID of a forget command
 PendingConfirmations = Dict[int, ConfirmationBody]
