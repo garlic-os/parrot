c12d74d926e92a302347a57a7bd71be98aa32952
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-07-09 23:50:21 -0500

    Move bot instantiation to separate file

---
+++ bot.py
@@ -0,0 +1,53 @@
+from typing import List, Optional
+
+import os
+import logging
+from redis import Redis
+from discord import Activity, ActivityType, AllowedMentions
+from discord.ext.commands import AutoShardedBot
+
+
+class Parrot(AutoShardedBot):
+    def __init__(
+        self, *,
+        prefix: str,
+        owner_ids: List[int],
+        admin_role_ids: Optional[List[int]]=None,
+        redis: Redis,
+    ):
+        super().__init__(
+            command_prefix=prefix,
+            owner_ids=owner_ids,
+            case_insensitive=True,
+            allowed_mentions=AllowedMentions.none(),
+            activity=Activity(
+                name=f"everyone ({prefix}help)",
+                type=ActivityType.listening,
+            ),
+        )
+        self.admin_role_ids = admin_role_ids or []
+        self.redis = redis
+        self.load_extension("jishaku")
+        self.load_folder("cogs")
+        self.load_folder("events")
+        self.load_folder("commands")
+
+    def _list_filenames(self, directory: str) -> List[str]:
+        files = []
+        for filename in os.listdir(directory):
+            abs_path = os.path.join(directory, filename)
+            if os.path.isfile(abs_path):
+                filename = os.path.splitext(filename)[0]
+                files.append(filename)
+        return files
+
+    def load_folder(self, folder_name: str) -> None:
+        for module in self._list_filenames(folder_name):
+            path = f"{folder_name}.{module}"
+            try:
+                logging.info(f"Loading {path}... ")
+                self.load_extension(path)
+                logging.info("✅")
+            except Exception as error:
+                logging.info("❌")
+                logging.error(f"{error}\n")
--- main.py
+++ main.py
@@ -1,58 +1,26 @@
-from typing import List
-
 import os
 import logging
 import ujson as json  # ujson is faster
-from discord import Activity, ActivityType, AllowedMentions
-from discord.ext import commands
+from redis import Redis
 from dotenv import load_dotenv
+from bot import Parrot
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-prefix = os.environ.get("COMMAND_PREFIX", "|")
-
-print("Initializing bot...")
-bot = commands.AutoShardedBot(
-    command_prefix=prefix,
-    owner_ids=json.loads(os.environ["OWNERS"]),
-    case_insensitive=True,
-    allowed_mentions=AllowedMentions.none(),
-    activity=Activity(
-        name=f"everyone ({prefix}help)",
-        type=ActivityType.listening,
-    )
 )
 
-bot.admin_role_ids = json.loads(os.environ.get("ADMIN_ROLE_IDS", "[]"))
 
+logging.info("Initializing bot...")
+bot = Parrot(
+    prefix=os.environ.get("COMMAND_PREFIX", "|"),
+    owner_ids=json.loads(os.environ["OWNERS"]),
+    admin_role_ids=json.loads(os.environ.get("ADMIN_ROLE_IDS", "[]")),
+    redis=Redis(
+        host=os.environ["REDIS_HOST"],
+        port=int(os.environ["REDIS_PORT"]),
+        password=os.environ["REDIS_PASSWORD"],
+    ),
+)
 
-def load_folder(folder_name: str) -> None:
-    def list_filenames(directory: str) -> List[str]:
-        files = []
-        for filename in os.listdir(directory):
-            abs_path = os.path.join(directory, filename)
-            if os.path.isfile(abs_path):
-                filename = os.path.splitext(filename)[0]
-                files.append(filename)
-        return files
-
-    for module in list_filenames(folder_name):
-        path = f"{folder_name}.{module}"
-        try:
-            print(f"Loading {path}... ", end="")
-            bot.load_extension(path)
-            print("✅")
-        except Exception as e:
-            print("❌")
-            print(e, end="\n\n")
-
-
-bot.load_extension("jishaku")
-load_folder("cogs")
-load_folder("events")
-load_folder("commands")
-
-
-print("Logging in...")
 bot.run(os.environ["DISCORD_BOT_TOKEN"])
