2b0d2adf4f9504b184e77c9d63feaed6446e115c
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-07-10 00:21:03 -0500

    Wait for DB to finish loading before logging in
    If you start the bot and the database at the same time, the bot may log in and be able to make requests to the database before the database has finished loading its dataset into memory. This can be avoided by just not letting the bot log in until the database is ready to receive commands.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 
 import os
 import logging
+import time
 from redis import Redis
 from discord import Activity, ActivityType, AllowedMentions
 from discord.ext.commands import AutoShardedBot
@@ -51,3 +52,12 @@ class Parrot(AutoShardedBot):
             except Exception as error:
                 logging.info("❌")
                 logging.error(f"{error}\n")
+
+    def run(self, token: str, *, bot: bool, reconnect: bool) -> None:
+        redis_is_ready = self.redis.ping()
+        if not redis_is_ready:
+            logging.warn("Waiting for the database to finish loading...")
+        while not redis_is_ready:
+            time.sleep(1/10)
+            redis_is_ready = self.redis.ping()
+        return super().run(token, bot=bot, reconnect=reconnect)
