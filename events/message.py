dddd34510b62e49d497a8dc570375646d16c10a4
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-07-09 23:54:16 -0500

    Comment out broken "imitate-on-reply" feature

--- events/message.py
+++ events/message.py
@@ -42,29 +42,35 @@ class MessageEventHandler(commands.Cog):
             pass
 
         # Imitate again when someone replies to an imitate message.
-        # I really wish Python had optional chaining right now.
-        if (message.reference is not None and
-            message.reference.cached_message is not None and
-            message.reference.cached_message.webhook_id in self.bot.webhook_ids):
-            ctx = self.bot.get_context(message)
-            imitate_command = self.bot.get_command("imitate")
-            try:
-                await imitate_command.prepare(ctx)
-            except CommandError as error:
-                await imitate_command.dispatch_error(ctx, error)
-            else:
-                # HACK: Get the ID of the user Parrot imitated in this message
-                # through the message's AVATAR URL.
-                # Really relying on implementation quirks here, but I don't want
-                # to put in the effort to, like, log which messages imitate who
-                # or something like that to be able to do this properly.
-                # If Discord ever changes the structure of their avatar URLs or
-                # if I change Parrot to use different avatars, this will
-                # probably break. But I guess now we'll just cross that bridge
-                # when it comes.
-                avatar_url = str(message.reference.cached_message.author.avatar_url)
-                user_id = avatar_url.split("/")[2]
-                await imitate_command(ctx, user_id)
+        # if message.reference is not None:
+        #     src_message = await message.channel.fetch_message(
+        #         message.reference.message_id
+        #     )
+        #     if (src_message is not None and
+        #         src_message.webhook_id is not None and
+        #         src_message.channel.id in self.bot.speaking_channels):
+        #         try:
+        #             # HACK: Get the ID of the user Parrot imitated in this
+        #             # message through the message's AVATAR URL.
+        #             # Really relying on implementation quirks here, but I don't
+        #             # want to put in the effort to, like, log which messages
+        #             # imitate who or something like that to be able to do this
+        #             # properly.
+        #             # If Discord ever changes the structure of their avatar URLs
+        #             # or if I change Parrot to use different avatars, this will
+        #             # probably break. But I guess now we'll just cross that
+        #             # bridge when it comes.
+        #             avatar_url = str(src_message.author.avatar_url)
+        #             user_id = avatar_url.split("/")[4]
+        #             message.content = user_id
+        #             ctx = await self.bot.get_context(message)
+        #             imitate_command = self.bot.get_command("imitate")
+        #             await imitate_command.prepare(ctx)
+        #             await imitate_command(ctx, user_id)
+        #         except Exception as e:
+        #             # Don't try to make amends if this feature fails. It isn't
+        #             # that important.
+        #             pass
 
 
 
