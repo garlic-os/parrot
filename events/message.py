14a24f7a51ed4b797b05113595800fd50f992ce9
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-07-13 20:57:09 -0500

    Make learning accessible to Quickstart again

--- events/message.py
+++ events/message.py
@@ -34,7 +34,7 @@ class MessageEventHandler(commands.Cog):
         # Ignore NotRegisteredError errors; Parrot shouldn't learn from
         #   non-registered users, anyway.
         try:
-            learned_count = self.learn_from(message)
+            learned_count = self.bot.learn_from(message)
             tag = f"{message.author.name}#{message.author.discriminator}"
 
             # Log results.
@@ -75,79 +75,6 @@ class MessageEventHandler(commands.Cog):
         #             # Don't try to make amends if this feature fails. It isn't
         #             # that important.
         #             pass
-
-
-    def validate_message(self, message: Message) -> bool:
-        """
-        A message must pass all of these checks
-            before Parrot can learn from it.
-        """
-        content = message.content
-
-        return (
-            # Text content not empty.
-            # mypy giving some nonsense error that doesn't occur in runtime
-            len(content) > 0 and  # type: ignore
-
-            # Not a Parrot command.
-            not content.startswith(self.bot.command_prefix) and
-
-            # Only learn in text channels, not DMs.
-            message.channel.type == ChannelType.text and
-
-            # Most bots' commands start with non-alphanumeric characters, so if
-            # a message starts with one other than a known Markdown character or
-            # special Discord character, Parrot should just avoid it because
-            # it's probably a command.
-            (
-                content[0].isalnum() or
-                regex.discord_string_start.match(content[0]) or
-                regex.markdown.match(content[0])
-            ) and
-
-            # Don't learn from self.
-            message.author.id != self.bot.user.id and
-
-            # Don't learn from Webhooks.
-            not message.webhook_id and
-
-            # Parrot must be allowed to learn in this channel.
-            message.channel.id in self.bot.learning_channels and
-
-            # People will often say "v" or "z" on accident while spamming;
-            # they don't like when Parrot learns from those mistakes.
-            content not in ("v", "z")
-        )
-
-
-    def learn_from(self, messages: Union[Message, List[Message]]) -> int:
-        """
-        Add a Message or array of Messages to a user's corpus.
-        Every Message in the array must be from the same user.
-        """
-        # Ensure that messages is a list.
-        # If it's not, make it a list with one value.
-        if not isinstance(messages, list):
-            messages = [messages]
-
-        user = messages[0].author
-
-        # Every message in the array must have the same author, because the
-        # Corpus Manager adds every message passed to it to the same user.
-        for message in messages:
-            if message.author != user:
-                raise ValueError("Too many authors; every message in a list passed to learn_from() must have the same author.")
-
-        # Only keep messages that pass all of validate_message()'s checks.
-        messages = list(filter(self.validate_message, messages))
-
-        # Add these messages to this user's corpus and return the number of
-        # messages that were added.
-        if len(messages) > 0:
-            return self.bot.corpora.add(user, messages)
-        return 0
-
-
 
 
 def setup(bot: Parrot) -> None:
