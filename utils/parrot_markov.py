b7a7fcf9310c87f4bdd0188f4d001e04da2bfed3
the-garlic-os <14845347+the-garlic-os@users.noreply.github.com>
2022-07-31 18:37:48 -0500

    Sometimes don't accept unoriginal gibberish

--- utils/parrot_markov.py
+++ utils/parrot_markov.py
@@ -27,9 +27,22 @@ class GibberishMarkov(markovify.Text):
             retain_original=False,
             well_formed=False,
         )
+        self.original = text
+
+    def word_join(self, words: List[str]) -> str:
+        # The generator usually puts spaces between each entry in the list because
+        # it expects them to be words. Since they're actually characters here,
+        # we join the list without spaces.
+        # I could be smarter about this and make it use a string instead of a
+        # list of strings, but I would have to modify markovify.Chain to do that
+        # and I don't want to!
+        return "".join(words)
 
     def make_sentence(self, init_state=None, **kwargs):
-        # The generator puts spaces between each character now because it thinks
-        # they're words. To band-aid this, we can just remove every other
-        # character to take out the extra spaces.
-        return super().make_sentence(init_state=init_state, **kwargs)[::2]
+        # Make some gibberish. If it ends up the same as the original text,
+        # maybe try again.
+        acceptable = False
+        while not acceptable:
+            sentence = super().make_sentence(init_state=init_state, **kwargs)
+            acceptable = sentence != self.original or random.random() < 0.5
+        return sentence
