d55fe1c14867883375629e3960f7ae640ba002bc
the-garlic-os <14845347+the-garlic-os@users.noreply.github.com>
2022-10-04 19:45:23 +0000

    Handle NotFound error

--- utils/fetch_webhook.py
+++ utils/fetch_webhook.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from discord import Forbidden, HTTPException, Webhook
+from discord import Forbidden, HTTPException, NotFound, Webhook
 from discord.ext.commands import Context
 
 
@@ -10,13 +10,18 @@ async def fetch_webhook(ctx: Context) -> Optional[Webhook]:
         (ctx.channel.id,),
     ).fetchone()
     if res is not None and res[0] is not None:
-        return await ctx.bot.fetch_webhook(res[0])
+        try:
+            return await ctx.bot.fetch_webhook(res[0])
+        except NotFound:
+            # Saved webhook ID is invalid; make a new one
+            pass
 
-    # If not, create one.
+    # Parrot does not have a webhook for this channel, so create one.
     try:
         webhook = await ctx.channel.create_webhook(
             name=f"Parrot in #{ctx.channel.name}",
             avatar=(await ctx.bot.user.avatar.read()),
+            reason="Automatically created by Parrot",
         )
         ctx.bot.db.execute(
             "UPDATE channels SET webhook_id = ? WHERE id = ?;",
@@ -26,6 +31,7 @@ async def fetch_webhook(ctx: Context) -> Optional[Webhook]:
     except (Forbidden, HTTPException, AttributeError):
         # - Forbidden: Parrot lacks permission to make webhooks here.
         # - AttributeError: Cannot make a webhook in this type of channel, like
-        # a DMChannel.
-        # - HTTPException: something is very wrong
+        #   a DMChannel.
+        # - HTTPException: 400 Bad Request; there is already the maximum number
+        #   of webhooks allowed in this channel (10).
         return None
