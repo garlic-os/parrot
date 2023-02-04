1c2b51c8cced1249932122e46d727156b48b46f9
the-garlic-os <14845347+garlic-os@users.noreply.github.com>
2023-02-04 17:14:19 -0600

    Add |link command
    Send a random link the user has sent before.
    GitHub Copilot wrote nearly all of this.

--- utils/regex.py
+++ utils/regex.py
@@ -3,3 +3,4 @@ import re
 markdown = re.compile(r"[*_`~]")
 discord_string_start = re.compile(r"[<@:]")
 do_not_capitalize = re.compile(r"(^<.*>$)|(^.+:\/\/.+$)")
+url = re.compile(r"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$")
