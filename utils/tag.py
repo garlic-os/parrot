fdc13c14717f95e2be703e14f1ce64bb2311d441
Nate <14845347+garlic-os@users.noreply.github.com>
2023-08-18 17:59:30 -0300

    Fix tag()
    Oops! discriminator isn't a number why would I expect a piece of COMPLETELY NUMERICAL INFORMATION to be a number
    Also now respects global nicknames

--- utils/tag.py
+++ utils/tag.py
@@ -2,6 +2,8 @@ from discord import User
 
 
 def tag(user: User) -> str:
-    if user.discriminator == 0:
+    if user.discriminator != "0":
+        return f"@{user.name}#{user.discriminator}"
+    if user.global_name is None:
         return f"@{user.name}"
-    return f"@{user.name}#{user.discriminator}"
+    return f"@{user.global_name}"
