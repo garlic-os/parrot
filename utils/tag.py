43c3d1804931aa859dbcbe2f9bed9df85d0515b9
Nate <14845347+garlic-os@users.noreply.github.com>
2023-08-18 18:09:37 -0300

    Wait don't use nicknames

--- utils/tag.py
+++ utils/tag.py
@@ -4,6 +4,4 @@ from discord import User
 def tag(user: User) -> str:
     if user.discriminator != "0":
         return f"@{user.name}#{user.discriminator}"
-    if user.global_name is None:
-        return f"@{user.name}"
-    return f"@{user.global_name}"
+    return f"@{user.name}"
