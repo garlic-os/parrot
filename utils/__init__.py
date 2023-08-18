cda4fd48f80f57d9c7d8865f33a456d24eff0123
Nate <14845347+garlic-os@users.noreply.github.com>
2023-08-18 19:22:26 -0300

    Add executor_function to __all__

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from utils.tag import tag
+from utils.executor_function import executor_function
 from utils.fetch_webhook import fetch_webhook
 from utils.history_crawler import HistoryCrawler
 from utils.parrot_embed import ParrotEmbed
@@ -6,6 +7,7 @@ from utils.parrot_markov import GibberishMarkov, ParrotMarkov
 
 
 __all__ = [
+    "executor_function",
     "fetch_webhook",
     "HistoryCrawler",
     "GibberishMarkov", "ParrotMarkov",
