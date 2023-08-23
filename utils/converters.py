c125664e141f6fee5b8d14f06ec705d28dec889c
Nate <14845347+garlic-os@users.noreply.github.com>
2023-08-23 16:27:20 -0300

    feat(text): add ability to imitate "someone"
    Randomly chooses a registered user in the server to imitate.
    I ended up refactoring the Userlike converter into a different levels of "fuzziness". BaseUserlike is just mentions and IDs. Userlike adds "me". FuzzyUserlike adds "you" and "someone" -- for fun!
    
    This also REMOVES "you" from the default Userlike behavior. There are a lot of other commands where "you" doesn't make sense, so that's why I thought I should do that.

--- utils/converters.py
+++ utils/converters.py
@@ -1,13 +1,53 @@
-import re
-from typing import Union
+from typing import Optional, Set, Union
 from discord.ext import commands
 from discord import Member, User
 
 from discord.errors import NotFound
 from utils.exceptions import UserNotFoundError
 
+import re
+import random
 
-class Userlike(commands.Converter):
+
+class BaseUserlike(commands.Converter):
+    def __init__(self):
+        self._checks = []
+
+    def _user_not_found(text: str) -> UserNotFoundError:
+        return UserNotFoundError(f'User "{text}" does not exist.')
+
+    async def convert(self, ctx: commands.Context, text: str=None) -> Union[User, Member]:
+        if text is None:
+            raise self._user_not_found(text)
+
+        text = text.lower()
+
+        for check in self._checks:
+            result = check(ctx, text)
+            if result is not None:
+                return result
+
+        # If this is not a guild, it must be a DM channel, and therefore the
+        # only person you can imitate is yourself.
+        if ctx.guild is None:
+            raise self._user_not_found(text)
+
+        # Strip the mention down to an ID.
+        try:
+            user_id = int(re.sub("[^0-9]", "", text))
+        except ValueError:
+            raise self._user_not_found(text)
+
+        # Fetch the member by ID.
+        try:
+            return await ctx.guild.fetch_member(user_id)
+        except NotFound:
+            raise self._user_not_found(text)
+
+        raise self._user_not_found(text)
+
+
+class Userlike(BaseUserlike):
     """
     A string that can resolve to a User.
     Works with:
@@ -15,25 +55,22 @@ class Userlike(commands.Converter):
         - User IDs, like 394750023975409309
         - The string "me" or "myself", which resolves to the context's author
         - The string "you", "yourself", or "previous" which resolves to the last
-              person to speak in the channel
+            person who spoke in the channel
     """
+    def __init__(self):
+        self._checks.append(self._me)
 
-    async def convert(
-        self,
-        ctx: commands.Context,
-        text: str=None
-    ) -> Union[User, Member]:
-        # Use this error if anything goes wrong.
-        user_not_found = UserNotFoundError(f'User "{text}" does not exist.')
-
-        if text is None:
-            raise user_not_found
-
-        text = text.lower()
-
+    def _me(self, ctx: commands.Context, text: str) -> Optional[Union[User, Member]]:
         if text in ("me", "myself"):
             return ctx.author
 
+
+class FuzzyUserlike(Userlike):
+    def __init__(self):
+        self._checks.append(self._you)
+        self._checks.append(self._someone)
+
+    def _you(self, ctx: commands.Context, text: str) -> Optional[Union[User, Member]]:
         # Get the author of the last message send in the channel who isn't
         # Parrot or the person who sent this command.
         if text in ("you", "yourself", "previous"):
@@ -52,19 +89,10 @@ class Userlike(commands.Converter):
                         return await ctx.guild.fetch_member(message.author.id)
                     return message.author
 
-        # If this is not a guild, it must be a DM channel, and therefore the
-        #   only person you can imitate is yourself.
-        if ctx.guild is None:
-            raise user_not_found
-
-        # Strip the mention down to an ID.
-        try:
-            user_id = int(re.sub("[^0-9]", "", text))
-        except ValueError:
-            raise user_not_found
-
-        # Fetch the member by ID.
-        try:
-            return await ctx.guild.fetch_member(user_id)
-        except NotFound:
-            raise user_not_found
+    # Choose a random registered user in this channel.
+    def _someone(self, ctx: commands.Context, text: str) -> Optional[Union[User, Member]]:
+        if text in ("someone", "somebody", "anyone", "anybody"):
+            if ctx.guild is None:
+                return ctx.author
+            registered_users_here = ctx.bot.registered_users.intersection(ctx.channel.members)
+            return random.choice(registered_users_here)
