8ed30a07b6ac34f9bacfe96fb32e9b52fb1b8cf1
Nate <14845347+garlic-os@users.noreply.github.com>
2023-08-23 19:58:09 -0300

    Improve debug loggin

--- database/avatar_manager.py
+++ database/avatar_manager.py
@@ -7,6 +7,7 @@ from discord import File, TextChannel, User
 from discord.errors import NotFound
 import config
 from utils.image import modify_avatar
+from utils import tag
 
 
 class AvatarManager:
@@ -47,9 +48,9 @@ class AvatarManager:
             # User hasn't changed their avatar since last time they did
             # |imitate, so we can use the cached modified avatar.
             if self._avatar_url_id(user.display_avatar.url) == self._avatar_url_id(original_avatar_url):
-                logging.debug("[AVATARS] CACHE HIT")
+                logging.debug(f"[AVATARS] CACHE HIT {tag(user)}")
                 return modified_avatar_url
-            logging.debug("[AVATARS] CACHE MISS; data is not None")
+            logging.debug(f"[AVATARS] CACHE MISS {tag(user)}; data is not None")
 
             # Else, user has changed their avatar.
             # Respect the user's privacy by deleting the message with their old
@@ -59,7 +60,7 @@ class AvatarManager:
                 self._delete_message(avatar_channel, modified_avatar_message_id)
             )
         else:
-            logging.debug("[AVATARS] CACHE MISS; data is none")
+            logging.debug(f"[AVATARS] CACHE MISS {tag(user)}; data is none")
 
         # User has changed their avatar since last time they did |imitate or has
         # not done |imitate before, so we must create a modified version of
