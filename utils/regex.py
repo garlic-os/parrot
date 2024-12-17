eb41d8ebc40642c90f9d9c84f03551c526fba252
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-12-17 15:13:05 -0600

    Clean slate

--- utils/regex.py
+++
@@ -1,5 +0,0 @@
-import re
-
-markdown = re.compile(r"[*_`~]")
-discord_string_start = re.compile(r"[<@:]")
-do_not_capitalize = re.compile(r"(^<.*>$)|(^.+:\/\/.+$)")
--- utils/tag.py
+++
@@ -1,7 +0,0 @@
-from discord import Member, User
-
-
-def tag(user: User | Member) -> str:
-    if user.discriminator != "0":
-        return f"@{user.name}#{user.discriminator}"
-    return f"@{user.name}"
