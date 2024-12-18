782550ad5552db017693d252ce19cfd687cc0cb3
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-12-18 00:29:30 -0600

    Fix typo

--- parrot/db/models.py
+++ parrot/db/models.py
@@ -8,14 +8,14 @@ class User(SQLModel, table=True):
 	is_registered: bool = False
 	original_avatar_url: str | None
 	modified_avatar_url: str | None
-	original_avatar_message_id: Snowflake | None
+	original_avatar_message_id: Snowflake | None = None
 
 
 class Channel(SQLModel, table=True):
 	id: Snowflake = Field(primary_key=True)
 	can_speak_here: bool = False
 	can_learn_here: bool = False
-	webhook_id: Snowflake | None
+	webhook_id: Snowflake | None = None
 
 
 class Message(SQLModel, table=True):
