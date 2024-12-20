110222452bb09c62325277ec9d351495c23412fc
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-12-20 02:25:48 -0600

    Make garlic-os want to learn alembic

--- parrot/db/models.py
+++ parrot/db/models.py
@@ -1,16 +1,9 @@
+import datetime as dt
 from enum import Enum
 
 from sqlmodel import Field, SQLModel
 
 from parrot.core.types import Snowflake
-
-
-class User(SQLModel, table=True):
-	id: Snowflake = Field(primary_key=True)
-	is_registered: bool = False
-	original_avatar_url: str | None
-	modified_avatar_url: str | None
-	original_avatar_message_id: Snowflake | None = None
 
 
 class Channel(SQLModel, table=True):
@@ -27,11 +20,17 @@ class Channel(SQLModel, table=True):
 class Message(SQLModel, table=True):
 	id: Snowflake = Field(primary_key=True)
 	user_id: Snowflake = Field(foreign_key="User.id")
-	timestamp: Snowflake
+
+	# TODO: Migration for this too!!!!
+	guild_id: Snowflake = Field(foreign_key="Guild.id")
+
+	timestamp: dt.datetime  # TODO: migrationg Snowflake → datetime
 	content: str
 
 
 class GuildMeta(Enum):
+	"""Extracted out so these can be used on their own elsewhere"""
+
 	default_imitation_prefix = "Not "
 	default_imitation_suffix = ""
 
@@ -40,3 +39,24 @@ class Guild(SQLModel, table=True):
 	id: Snowflake = Field(primary_key=True)
 	imitation_prefix: str = GuildMeta.default_imitation_prefix.value
 	imitation_suffix: str = GuildMeta.default_imitation_suffix.value
+
+
+class User(SQLModel, table=True):
+	id: Snowflake = Field(primary_key=True)
+	# is_registered: bool = False  # TODO: mfmmmgmgration delete this
+	# original_avatar_url: str | None  # TODO: migration delete these too
+	# modified_avatar_url: str | None
+	# original_avatar_message_id: Snowflake | None = None
+	wants_random_devolve: bool = True
+
+# TODO: rahhhhhh migration!!!!!
+class Registration(SQLModel, table=True):
+	guild_id: Snowflake = Field(foreign_key="Guild.id")
+	user_id: Snowflake = Field(foreign_key="User.id")
+
+	# this user information moved to server-specific table now that avatars can
+	# be server-specific
+	# TODO: optimize for users who don't use server-specific avatars
+	original_avatar_url: str | None
+	modified_avatar_url: str | None
+	original_avatar_message_id: Snowflake | None = None
