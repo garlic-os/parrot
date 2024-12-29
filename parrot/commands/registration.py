b8af8d5d0784d514236b2fd9355d2129f5b564a9
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-12-28 23:46:04 -0600

    Create registration.py

---
+++ parrot/commands/registration.py
@@ -0,0 +1,126 @@
+from typing import cast
+
+import discord
+from discord.ext import commands
+
+from bot import Parrot
+from parrot.core.exceptions import UserPermissionError
+from parrot.utils import ParrotEmbed
+from parrot.utils.converters import Memberlike
+
+
+class Registration(commands.Cog):
+	def __init__(self, bot: Parrot):
+		self.bot = bot
+
+
+	@commands.command(
+		aliases=["agree", "accept", "initiate", "initate"],
+		brief="Register with Parrot."
+	)
+	@commands.cooldown(2, 4, commands.BucketType.user)
+	@commands.guild_only()
+	async def register(self, ctx: commands.Context, who: Memberlike | None=None) -> None:
+		""" Register to let Parrot imitate you. """
+		# Pylance doesn't get along with `commands.Converter`s
+		who_ = cast(discord.Member | None, who)
+		if who_ is None:
+			who_ = cast(discord.Member, ctx.author)
+		elif who_.id != ctx.author.id:
+			raise UserPermissionError("You can only register yourself.")
+
+		# Update the "is_registered" field on this user in the database.
+		self.bot.crud.member.set_registered(who_, True)
+
+		embed = ParrotEmbed(
+			title="✅ Registered!",
+			color_name=ParrotEmbed.Color.Green,
+			description=(
+				"Now Parrot can start learning your speech patterns and "
+				"imitate you."
+			)
+		)
+		embed.add_field(
+			name="Tip:",
+			value=(
+				f"Try the `{self.bot.command_prefix}quickstart` command to "
+				"immediately give Parrot a dataset to imitate you from! It "
+				"will scan your past messages to create a model of how you "
+				"speak so you can start using Parrot right away."
+			)
+		)
+
+		await ctx.send(embed=embed)
+
+	@commands.command(
+		aliases=["disagree", "unaccept", "uninitiate", "uninitate"],
+		brief="Unregister with Parrot."
+	)
+	@commands.cooldown(2, 4, commands.BucketType.user)
+	@commands.guild_only()
+	async def unregister(
+		self,
+		ctx: commands.Context,
+		who: Memberlike | None=None
+	) -> None:
+		"""
+		Remove your registration from Parrot.
+		Parrot will stop collecting your messages and will not be able to
+		imitate you until you register again.
+		"""
+		who_ = cast(discord.Member | None, who)
+		if who_ is None:
+			who_ = cast(discord.Member, ctx.author)
+		elif who_.id != ctx.author.id:
+			raise UserPermissionError("You can only unregister yourself.")
+
+		self.bot.crud.member.set_registered(who_, False)
+
+		embed = ParrotEmbed(
+			title="Unregistered!",
+			color_name=ParrotEmbed.Color.Gray,
+			description=(
+				"Parrot will no longer be able to imitate you and it "
+				"has stopped collecting your messages.\n\n_If you're done with "
+				"Parrot and don't want it to have your messages anymore, or if "
+				"you just want a fresh start, you can do "
+				f"`{self.bot.command_prefix}forget me` and your existing data "
+				"will be permanently deleted from Parrot._"
+			)
+		)
+
+		await ctx.send(embed=embed)
+
+	@commands.command(
+		aliases=[
+			"registration_status",
+			"registrationstatus",
+			"registration",
+			"checkregistration",
+			"check_registration",
+		],
+		brief="Check if you're registered with Parrot.",
+	)
+	@commands.cooldown(2, 4, commands.BucketType.user)
+	@commands.guild_only()
+	async def status(self, ctx: commands.Context, who: Memberlike | None=None) -> None:
+		"""
+		Check if you're registered with Parrot.
+		You need to be registered for Parrot to be able to analyze your messages
+		and imitate you.
+		"""
+		who_ = cast(discord.Member | None, who)
+		if who_ is None:
+			who_ = cast(discord.Member, ctx.author)
+		subject_verb = "You are" if who_.id == ctx.author.id else f"{who_.mention} is"
+		if who_.bot:
+			await ctx.send("✅ Bots do not need to be registered.")
+			return
+		if self.bot.crud.member.is_registered(who_):
+			await ctx.send(f"✅ {subject_verb} currently registered with Parrot.")
+		else:
+			await ctx.send(f"❌ {subject_verb} not currently registered with Parrot.")
+
+
+async def setup(bot: Parrot) -> None:
+	await bot.add_cog(Registration(bot))
