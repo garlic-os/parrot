e63df39bab0887895605951e9e95ee0ebd6e6589
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-12-28 23:31:35 -0600

    Create misc.py

---
+++ parrot/commands/misc.py
@@ -0,0 +1,51 @@
+from discord.ext import commands
+
+from bot import Parrot
+from parrot.utils import ParrotEmbed, cast_not_none, tag
+
+
+class Miscellaneous(commands.Cog):
+	def __init__(self, bot: Parrot):
+		self.bot = bot
+
+	@commands.command()
+	@commands.cooldown(2, 4, commands.BucketType.user)
+	async def ping(self, ctx: commands.Context) -> None:
+		""" Get the bot's reponse time. """
+		await ctx.send(
+			f"NEED PING??? Took **{round(self.bot.latency * 1000, 2)}** ms"
+		)
+
+	@commands.command(
+		aliases=["about", "bio", "code", "github", "source", "sourcecode"]
+	)
+	@commands.cooldown(2, 4, commands.BucketType.user)
+	async def info(self, ctx: commands.Context) -> None:
+		""" Get info about Parrot. """
+		garlic_os = await self.bot.fetch_user(206235904644349953)
+
+		bio = (
+			f"Parrot can imitate everyone. Do `{self.bot.command_prefix}imitate"
+			"me` to have Parrot imitate you, or do"
+			f"`{self.bot.command_prefix}help` to see what else Parrot can do."
+		)
+
+		embed = ParrotEmbed(
+			title="🦜 Parrot",
+			description=bio,
+		)
+		embed.add_field(
+			name="GitHub",
+			value="https://github.com/garlic-os/parrot",
+		)
+		embed.set_thumbnail(url=cast_not_none(self.bot.user).display_avatar.url)
+		embed.set_footer(
+			text=f"Parrot v2 • Made by {tag(garlic_os)}",
+			icon_url=garlic_os.display_avatar.url,
+		)
+
+		await ctx.send(embed=embed)
+
+
+async def setup(bot: Parrot) -> None:
+	await bot.add_cog(Miscellaneous(bot))
